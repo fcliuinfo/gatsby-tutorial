d52521f8532dc0dbbb91030285f940487d3ffe7b
Aurora Liu <fcliu.dev@gmail.com>
2022-07-11 21:02:07 +0800

    add images

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+
+const AboutPage = () => {
+  return (
+    <main>
+      <title>About Page</title>
+      <h1>About Me</h1>
+      <Link to="/">Back to Home</Link>
+      <p>Hi there! I'm the proud creator of this site, which I build with Gatsby.</p>
+    </main>
+  )
+}
+
+export default AboutPage
